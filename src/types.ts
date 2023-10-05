7dabec47b01b47b0d14831472c5da36d1bc2814e
starlingk21 <starlingk21@gmail.com>
2023-10-05 22:53:36 +0300

    [dev_feature-visma-books] Created Book interface

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
 // This only exists so types.ts is always recognized as a module
 // Add your custom types and remove this Dummy type when you're done
-export interface Dummy {
-
+export interface Book {
+  title?: string;
+  author?: string;
+  isbn?: string;
+  cover?: string;
 }
